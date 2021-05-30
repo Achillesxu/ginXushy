0aa5b4e204f30acf193d5a264d0ec61b476c0fc5
xushiyin <yuqingxushiyin@gmail.com>
2021-05-30 11:34:56 +0800

    feat: add bind login

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ import (
 
 var db = make(map[string]string)
 
+// Login Binding from JSON
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func addXuRoutes(rg *gin.RouterGroup) {
 	xu := rg.Group("xu")
 	xu.GET("/comments", func(c *gin.Context) {
@@ -73,11 +79,42 @@ func setupRouter() *gin.Engine {
 			c.JSON(http.StatusOK, gin.H{"status": "ok"})
 		}
 	})
+
 	authorized.GET("comments", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "users comments")
 	})
 	authorized.GET("pictures", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "users pictures")
+	})
+
+	r.POST("/loginJson", func(c *gin.Context) {
+		var j Login
+		if err := c.ShouldBindJSON(&j); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if j.User != "manu" || j.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// Example for binding a HTML form (user=manu&password=123)
+	r.POST("/loginForm", func(c *gin.Context) {
+		var form Login
+		// This will infer what binder to use depending on the content-type header.
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if form.User != "manu" || form.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 
 	return r
